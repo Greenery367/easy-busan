df4765620a2ba987f5e85149705ab8b7d9f5c703
kyeonghooon <gibbon0510@gmail.com>
2024-08-08 20:49:45 +0900

    프로젝트 세팅
    data.sql 주석 해제 필요

---
+++ src/main/java/com/easybusan/EasyBusanApplication.java
@@ -0,0 +1,13 @@
+package com.easybusan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyBusanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyBusanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/easybusan/EasyBusanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easybusan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyBusanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
